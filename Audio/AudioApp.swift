afe437a99edf6d7b34bfa097630f8b6d359339ba
GuruMahan <iosdeve2022@gmail.com>
2023-02-02 19:06:17 +0530

    Initial Commit

---
+++ Audio/AudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioApp.swift
+//  Audio
+//
+//  Created by Guru Mahan on 02/01/23.
+//
+
+import SwiftUI
+
+@main
+struct AudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AudioView()
+        }
+    }
+}
